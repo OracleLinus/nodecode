b48cb53af6b1512f84dd423755a0b1eb61497fae
OracleLinus <33482049+OracleLinus@users.noreply.github.com>
2018-06-28 14:46:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var app = express();
 
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
-runGetRequest();
+for (var i = 0; i < 10; i++) {
+    runGetRequest(i);
+}
+
 
 // Does a GET request to ip.jsontest.com
-function runGetRequest() {
+function runGetRequest(var i) {
     
     //sample URL.
-    var url = "http://129.157.179.180:3000/shield/33/45/yellow/linusNode";
+    var url = "http://129.157.179.180:3000/fighters/45/" + i + "/yellow/linusNode";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
